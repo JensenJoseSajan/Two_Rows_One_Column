a054aac751a106606be9101a5e18b0820bc43ded
JJS123466 <144385804+JJS123466@users.noreply.github.com>
2023-12-11 06:03:05 +0530

    Initial Commit

---
+++ Calculator.c
@@ -0,0 +1,37 @@
+//Program for a Calculator (with basic functions and Matrix calculation)
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int star; //used for line breaks
+    int A; //used for switch 1
+    
+    for (star=20; star > 0; star--)
+    {
+        printf ("* * ");
+    }
+    printf ("\n");
+
+    //The Welcoming statement.
+    printf("\nWelcome to the Basic Calculator by team 'Two_Rows_One_Column'\n");
+    printf("In this calculator, you can run programs like basic algebra, trignometric functions, factorial functions and matrix functions\n\n");
+
+    for (star=20; star > 0; star--)
+    {
+        printf ("* * ");
+    }
+    printf ("\n");
+    
+    switch(A)
+    {
+        case 1 : //Basic Algebra //Devdarsh's Part
+
+        case 2 : //Trigonometroc Functions //Snith and Affan's Part
+
+        case 3 : //Factorial //Niranjan's Part
+
+        case 4 : //Matrix Functions //Jensen and Raaj's Part
+    }
+
+    return 0;
+}

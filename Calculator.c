81c274a3d84495e862bed877e8cb95c6f86872d4
JJS123466 <144385804+JJS123466@users.noreply.github.com>
2023-12-11 20:22:23 +0530

    Edited

--- Calculator.c
+++ Calculator.c
@@ -6,7 +6,7 @@ int CP(int);
 int main()
 {
     int star; //used for line breaks
-    int A; //used for switch 1
+    int A,M; //used for switch 1
     
     for (star=20; star > 0; star--)
     {
@@ -31,74 +31,65 @@ int main()
         case 2 : //Trigonometroc Functions //Snith and Affan's Part
     
         case 3 : //Factorial //Niranjan's
-       int main()
-{
-     int M;
-    printf("Enter the menu number: ");
-    scanf("%d",M);
-    switch(M)
-    {
-        case 1:
-        int main()
-        {
-            int num,a,n;
-    printf("Enter the number:");
-    scanf("%d",&num);
-    a=fact(n);
-    printf("Factorial of the given number=%d",a);
-    }
-    int fact(int x)
-    {
-        int F;
-        if(x==0)
-        {
-            return 1;
-        }
-        else
-        {
-            F=x*fact(x-1);
-            return F;
-        }
-        break;
 
-    }
+        printf("Enter the menu number: ");
+        scanf("%d",M);
+        switch(M)
+        {
+            case 1:
+            int main()
+            {
+                int num,a,n;
+                printf("Enter the number:");
+                scanf("%d",&num);
+                a=fact(n);
+                printf("Factorial of the given number=%d",a);
+            }
+            int fact(int x)
+            {
+                int F;
+                if(x==0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    F=x*fact(x-1);
+                    return F;
+                }
+            break;
 
-        case 2:
-            int n,r,per,comb,f;
-            printf("Enter the value of n:");
-            scanf("%d",&n);
-            printf("Enter the value of r:");
-            scanf("%d",&r);
-            f=CP(n);
-            per=CP(n)/CP(n-r);
-            comb=per/CP(r);
-            printf("Permutation=%d",per);
-            printf("Combination+%d",comb);
+            }
+
+            case 2:
+                int n,r,per,comb,f;
+                printf("Enter the value of n:");
+                scanf("%d",&n);
+                printf("Enter the value of r:");
+                scanf("%d",&r);
+                f=CP(n);
+                per=CP(n)/CP(n-r);
+                comb=per/CP(r);
+                printf("Permutation=%d",per);
+                printf("Combination+%d",comb);
         }
         int CP(int y);
         {
            int F;
-        if(y==0)
-        {
-            return 1;
-        }
-        else
-        {
-            F=y*CP(y-1);
-            return F;
-        }
-        break;
-        default:
-        printf("invalid menu");
+            if(y==0)
+            {
+                return 1;
+            }
+            else
+            {
+                F=y*CP(y-1);
+                return F;
+            }
+            break;
+            default:
+            printf("Invalid Menu");
         }
         
-
-      
-
-
-    
-     
-
         case 4 : //Matrix Functions //Jensen and Raaj's Part
     }
 

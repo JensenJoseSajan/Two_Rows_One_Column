4e2262f575478c6fb22d9b23c12f51948ff659e9
snithshibu <snithshibu.b23ec1156@mbcet.ac.in>
2023-12-16 16:46:53 +0530

    added more cases to switch T

--- Calculator.c
+++ Calculator.c
@@ -171,12 +171,31 @@ int main()
      
               switch(T)
               {
-                 case 1 : 
-                          break;
-                 case 2 : 
-                          break;
-                 default : printf("Invalid Menu Number");
-                           break;
+               case 1 : 
+                       break;
+               case 2 : 
+                       break;
+               case 3 :
+                       break;
+               case 4:
+                        break;
+               case 5:
+                        break;
+               case 6:
+                        break;
+               case 7:
+                        break;
+               case 8:
+                        break;
+               case 9:
+                        break;
+               case 10:
+                        break;
+               case 11:
+                        break;
+               case 12:
+                        break;
+               default: printf("Invalid menu number");
               }
      //End of Snith's and Affan's Part
 
